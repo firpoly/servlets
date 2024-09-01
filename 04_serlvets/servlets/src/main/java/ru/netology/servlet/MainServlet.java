1d0492523f1a170ecec9d616479bfa685287485a
Firsova Polina <firpoly@gmail.com>
2024-09-02 00:57:18 +0300

    задание 1 исправления

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -9,50 +9,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
+    private PostController controller;
+    private static final String API_POST = "/api/posts";
+    private static final String API_POST_ID = "/api/posts/\\d+";
+    private static final String METHOD_DELETE = "/api/posts/\\d+";
+    private static final String METHOD_GET = "GET";
+    private static final String METHOD_POST = "POST";
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
-  public Long getPostId(String path) {
-    return Long.parseLong(path.substring(path.lastIndexOf("/")+1));
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = getPostId(path);
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = getPostId(path);
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
     }
-  }
+
+    public Long getPostId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(METHOD_GET) && path.equals(API_POST)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(METHOD_GET) && path.matches(API_POST_ID)) {
+                // easy way
+                final var id = getPostId(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(METHOD_POST) && path.equals(API_POST)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(METHOD_DELETE) && path.matches(API_POST_ID)) {
+                // easy way
+                final var id = getPostId(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
