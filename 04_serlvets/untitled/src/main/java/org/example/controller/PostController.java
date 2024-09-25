41c1b1b3413b54850c3433c9a95c46287a3376f0
Firsova Polina <firpoly@gmail.com>
2024-09-26 00:01:06 +0300

    домашняя работа- доработка

--- 04_serlvets/untitled/src/main/java/org/example/controller/PostController.java
+++ 04_serlvets/untitled/src/main/java/org/example/controller/PostController.java
@@ -5,8 +5,6 @@ import org.example.service.PostService;
 import org.springframework.web.bind.annotation.*;
 
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 @RestController
 @RequestMapping("/api/")
@@ -17,20 +15,20 @@ public class PostController {
         this.service = service;
     }
     @GetMapping("/posts")
-    public List<Post> all() throws IOException {
+    public List<Post> all() {
         return  service.all();
     }
     @GetMapping("/{id}")
-    public Post getById(@PathVariable long id, HttpServletResponse response) throws IOException {
+    public Post getById(@PathVariable long id)  {
         return service.getById(id);
 
     }
     @PostMapping("/posts")
-    public Post  save(@RequestBody  Post post) throws IOException {
+    public Post  save(@RequestBody  Post post) {
         return service.save(post);
     }
     @DeleteMapping("/{id}")
-    public void removeById(@PathVariable long id, HttpServletResponse response) throws IOException {
+    public void removeById(@PathVariable long id) {
         service.removeById(id);
     }
 }
